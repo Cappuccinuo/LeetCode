d502a2a4b30b2a4e613735e4dca7d44b384ecfb9
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-11 21:56:48 -0500

    453

--- Math/453.Minimum Moves to Equal Array Elements/README.md
+++ Math/453.Minimum Moves to Equal Array Elements/README.md
@@ -21,6 +21,12 @@ Only three moves are needed (remember each move increments two elements):
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 ```
 
+First sort the array. Record the smallest element, nums[0].
 
+We can find the pattern that count += nums[i] - nums[0]
+
+
+
+To reduce the time to sort, we can go through the nums array and record the minimum one, and go through the nums array again, add nums[i] - min.
 
 
--- Math/453.Minimum Moves to Equal Array Elements/Solution.java
+++ Math/453.Minimum Moves to Equal Array Elements/Solution.java
@@ -1,13 +1,26 @@
+//11ms
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(nums[i], min);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i] - min;
+        }
+        return count;
+    }
+}
+//42ms
 class Solution {
     public int minMoves(int[] nums) {
         int[] dp = new int[nums.length + 1];
         Arrays.sort(nums);
-        int smallest = nums[0];
-        dp[0] = 0;
-        dp[1] = 0;
-        for (int i = 2; i <= nums.length; i++) {
-            dp[i] = nums[i - 1] - smallest + dp[i - 1];
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            count = nums[i] - nums[0] + count;
         }
-        return dp[nums.length];
+        return count;
     }
 }
