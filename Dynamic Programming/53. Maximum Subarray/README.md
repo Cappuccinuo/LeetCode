1e383931881d13b13042cddc978ba54a92a2d00f
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-27 13:46:58 -0500

    README+ 53 Max Subarray

--- Dynamic Programming/53. Maximum Subarray/README.md
+++ Dynamic Programming/53. Maximum Subarray/README.md
@@ -18,3 +18,11 @@ Output: 6 ([4, -1, 2, 1])
 
 1. if current sum is smaller than 0, use nums[i]; otherwise sum = sum + nums[i]
 2. if the sum is larger than result, result = sum
+
+
+
+-9 -2 18 7 -6 4 9 -9 -5 0 5 -2 5 9 7
+
+If max = -9, dp[0] = -9. Then dp[1] = -2, cause dp[0] < 0, max = -2.
+
+Then dp[2] = 18, cause dp[1] = -2 < 0, max = 18….
--- Dynamic Programming/53. Maximum Subarray/Solution.java
+++ Dynamic Programming/53. Maximum Subarray/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
         int result = nums[0];
         int sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
