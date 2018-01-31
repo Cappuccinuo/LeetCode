c314500e60d652e3f6d604032e9dcba1a1a72249
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-31 16:20:59 -0500

    LC42 DP solution

--- Dynamic Programming/42. Trapping Rain Water/README.md
+++ Dynamic Programming/42. Trapping Rain Water/README.md
@@ -21,3 +21,12 @@ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 Solution 1 : 
 
 Brute force. For each element in the array, find the maximum level of water it can trap, which is equal to the minimum of maximum height of bars on both the sides minus its own height.
+
+Solution 2 :
+
+Instead iterate each element's rightmost and leftmost, we can store the left_most and right_most in two arrays. 
+
+- Find maximum height of bar from the left end upto an index i in the array left_max.
+- Find maximum height of bar from the right end upto an index i in the array right_max.
+- Iterate over the height array and update ans:
+- Add min(max_left[i],max_right[i])−height[i] to ansans
--- Dynamic Programming/42. Trapping Rain Water/Solution.java
+++ Dynamic Programming/42. Trapping Rain Water/Solution.java
@@ -1,6 +1,30 @@
 class Solution {
     public int trap(int[] height) {
         int size = height.length;
+        if (size == 0) {
+            return 0;
+        }
+        int result = 0;
+        int[] left_most = new int[size];
+        int[] right_most = new int[size];
+        left_most[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            left_most[i] = Math.max(left_most[i - 1], height[i]);
+        }
+        right_most[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            right_most[i] = Math.max(right_most[i + 1], height[i]);
+        } 
+        for (int i = 1; i < size - 1; i++) {
+            result += Math.min(right_most[i], left_most[i]) - height[i];
+        }
+        return result;
+    }
+}
+
+class Solution {
+    public int trap(int[] height) {
+        int size = height.length;
         int result = 0;
         for (int i = 0; i < size - 1; i++) {
             int max_left = 0;
