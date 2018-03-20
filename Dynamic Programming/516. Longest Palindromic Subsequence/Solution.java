d45ba263a9037bcd6a11f0275eaf75386e65a9d8
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-19 23:46:37 -0400

    README + 50 + 516

--- Dynamic Programming/516. Longest Palindromic Subsequence/Solution.java
+++ Dynamic Programming/516. Longest Palindromic Subsequence/Solution.java
@@ -1,3 +1,35 @@
+// 2018.3.19
+public class Solution {
+    /**
+     * @param s: the maximum length of s is 1000
+     * @return: the longest palindromic subsequence's length
+     */
+    public int longestPalindromeSubseq(String s) {
+        // write your code here
+        if (s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = 1;
+        }
+        for (int l = 2; l <= len; l++) {
+            for (int i = 0; i < len - l + 1; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][len - 1];
+    }
+}
+
+
 class Solution {
     public int longestPalindromeSubseq(String s) {
         int[][] dp = new int[s.length()][s.length()];
--- Math/50. Pow(x, n)/Solution.java
+++ Math/50. Pow(x, n)/Solution.java
@@ -1,3 +1,55 @@
+// 2018. 3. 19
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return recursiveHelper(x, N);
+    }
+    
+    public double recursiveHelper(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return recursiveHelper(x * x, n / 2);
+        }
+        else {
+            return x * recursiveHelper(x * x, n / 2);
+        }
+    }
+}
+
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+        double current = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                result = result * current;
+            }
+            current = current * current;
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
 class Solution {
     public double myPow(double x, int n) {
         if (n == 0) {
