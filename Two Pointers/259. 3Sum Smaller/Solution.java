e8c5d0447a7e261e34ad1f9d2131054c62a5ccaf
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-29 20:07:57 -0400

    README + 259

--- Two Pointers/259. 3Sum Smaller/Solution.java
+++ Two Pointers/259. 3Sum Smaller/Solution.java
@@ -1,3 +1,32 @@
+// 2018.3.29
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum < target) {
+                    count += right - left;
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
