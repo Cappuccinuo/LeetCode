c6096deb9c5e6ddde883394171383c871d543a13
Yuan Gao <gaoyuanbj@yahoo.com>
2018-04-09 22:11:57 -0600

    Solution 796

---
+++ String/796. Rotate String/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if (A.length() != B.length()) {
+            return false;
+        }
+        String str = A + A;
+        return str.indexOf(B) != -1;
+    }
+}
