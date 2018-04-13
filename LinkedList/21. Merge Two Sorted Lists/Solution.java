23b8a5cfb8ba2c2d8bce5dbf932166704f8fc3b6
Yuan Gao <gaoyuanbj@yahoo.com>
2018-04-12 23:49:00 -0400

    README + 21

--- LinkedList/21. Merge Two Sorted Lists/Solution.java
+++ LinkedList/21. Merge Two Sorted Lists/Solution.java
@@ -1,3 +1,45 @@
+// 2018.4.12
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        if (l1 == null) {
+            current.next = l2;
+        }
+        if (l2 == null) {
+            current.next = l1;
+        }
+        return dummy.next;
+    }
+}
+
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
