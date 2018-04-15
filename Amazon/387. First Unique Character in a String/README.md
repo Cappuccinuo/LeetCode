dd297e9626216ba426369fc739cca353c11a3d3d
Yuan Gao <gaoyuanbj@yahoo.com>
2018-04-15 14:49:18 -0400

    README + 387

--- Amazon/387. First Unique Character in a String/README.md
+++ Amazon/387. First Unique Character in a String/README.md
@@ -1,4 +1,4 @@
-Time spent : 5 min
+Time spent :30 min(self)
 
 Solution : String -> Integer	
 
@@ -27,5 +27,9 @@ Output: 2
 DESIGN STRATEGY: Use HashMap to store the times of each character, then traverse the string, if the time is 1, return the index.
 
 
+
+2018.4.15 Using Data Stream Method
+
+Use a hashmap to keep the prev node of current char, use a linked list to link all the unique characters so far, and use a hashset to record the duplicate characters.
 
 2018.4.13 Faster: We can traverse the 26 characters, for every character, we use indexOf to get its current index in the string, if it exist, we use lastIndexOf to get its last index in the string, compare the two, update the firstIndex variable.
--- Amazon/387. First Unique Character in a String/Solution.java
+++ Amazon/387. First Unique Character in a String/Solution.java
@@ -1,3 +1,73 @@
+// 2018.4.15
+class Solution {
+    class ListCharNode {
+        char val;
+        ListCharNode next;
+        ListCharNode(char x) {
+            this.val = x;
+        }
+    }
+    
+    class DataStream {
+        Set<Character> duplicate;
+        Map<Character, ListCharNode> nodeToPrev;
+        ListCharNode dummy;
+        ListCharNode tail;
+        
+        DataStream() {
+            duplicate = new HashSet();
+            nodeToPrev = new HashMap<>();
+            dummy = new ListCharNode('.');
+            tail = dummy;
+        }
+        
+        void add(char c) {
+            if (duplicate.contains(c)) {
+                return;
+            }
+            
+            if (nodeToPrev.get(c) == null) {
+                ListCharNode newNode = new ListCharNode(c);
+                tail.next = newNode;
+                nodeToPrev.put(c, tail);
+                tail = newNode;
+                return;
+            }
+            
+            ListCharNode prev = nodeToPrev.get(c);
+            prev.next = prev.next.next;
+            if (prev.next == null) {
+                tail = prev;
+            }
+            else {
+                nodeToPrev.put(prev.next.val, prev);
+            }
+            
+            duplicate.add(c);
+            nodeToPrev.remove(c);
+        }
+        
+        Character firstUniqueChar() {
+            if (dummy.next == null) {
+                return null;
+            }
+            return dummy.next.val;
+        }
+    }
+    
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        DataStream ds = new DataStream();
+        char[] letters = s.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            ds.add(letters[i]);
+        }
+        return ds.firstUniqueChar() == null ? -1 : s.indexOf(ds.firstUniqueChar());
+    }
+}
+
 // 2018.4.13
 class Solution {
     public int firstUniqChar(String s) {
