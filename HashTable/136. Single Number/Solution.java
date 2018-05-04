6652011b66ad53e56d8b8716f7d15d270ac6a378
Yuan Gao <gaoyuanbj@yahoo.com>
2018-05-04 11:46:07 -0400

    README + 136

---
+++ HashTable/136. Single Number/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
