b1a7d8fbb32af627776e71303b746a244c4d2b6c
Yuan Gao <gaoyuanbj@yahoo.com>
2018-05-25 04:19:55 -0400

    README + 79

--- Backtracking/79. Word Search/Solution.java
+++ Backtracking/79. Word Search/Solution.java
@@ -1,3 +1,51 @@
+// 2018.5.25
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0) {
+            return false;
+        }
+        if (word.length() == 0) {
+            return true;
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    boolean result = find(board, i, j, 0, word);
+                    if (result) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean find(char[][] board, int i, int j, int index, String word) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (!inBound(board, i, j) || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        board[i][j] = '#';
+        boolean result = find(board, i - 1, j, index + 1, word)
+            || find(board, i + 1, j, index + 1, word)
+            || find(board, i, j - 1, index + 1, word) 
+            || find(board, i, j + 1, index + 1, word);
+        board[i][j] = word.charAt(index);
+        return result;
+    }
+    
+    private boolean inBound(char[][] board, int i, int j) {
+        return i >= 0 && i < board.length && j >= 0 && j < board[0].length;
+    }
+}
+
+
+
 class Solution {
     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
