62aa365f5ecf993a91b83566f39163b61ab23535
Yuan Gao <gaoyuanbj@yahoo.com>
2018-06-06 01:28:33 -0400

    README + 207

--- Topological Sort/207. Course Schedule/Solution.java
+++ Topological Sort/207. Course Schedule/Solution.java
@@ -1,58 +1,40 @@
-// 2018.4.6 Topological Sort
 class Solution {
     Map<Integer, List<Integer>> map;
-    Map<Integer, Integer> inorder;
+    Map<Integer, Integer> inDegree;
+    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        map = new HashMap<Integer, List<Integer>>();
-        inorder = new HashMap<Integer, Integer>();
-        addPreToMap(prerequisites, numCourses);
-        getInDegree(prerequisites, numCourses);
-        for (Integer i : map.keySet()) {
-            System.out.println("i: " + i + ", list: " + map.get(i));
-        }
-        
-        for (Integer i : inorder.keySet()) {
-            System.out.println("i: " + i + ", list: " + inorder.get(i));
-        }
+        map = new HashMap<>();
+        inDegree = new HashMap<>();
+        getDegreeAndMapping(numCourses, prerequisites);
         
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (inorder.get(i) == 0) {
+            if (inDegree.get(i) == 0) {
                 queue.offer(i);
             }
         }
-        
         int count = 0;
         while (!queue.isEmpty()) {
             int course = queue.poll();
             count++;
-            List<Integer> pre = map.get(course);
-            for (int i = 0; i < pre.size(); i++) {
-                int current = pre.get(i);
-                inorder.put(current, inorder.get(current) - 1);
-                if (inorder.get(current) == 0) {
-                    queue.offer(current);
+            for (Integer i : map.get(course)) {
+                inDegree.put(i, inDegree.get(i) - 1);
+                if (inDegree.get(i) == 0) {
+                    queue.offer(i);
                 }
             }
         }
         return count == numCourses;
     }
     
-    public void addPreToMap(int[][] prerequisites, int numCourses) {
+    private void getDegreeAndMapping(int numCourses, int[][] prerequisites) {
         for (int i = 0; i < numCourses; i++) {
-            map.put(i, new ArrayList<Integer>());
+            map.put(i, new LinkedList<>());
+            inDegree.put(i, 0);
         }
         for (int i = 0; i < prerequisites.length; i++) {
+            inDegree.put(prerequisites[i][0], inDegree.get(prerequisites[i][0]) + 1);
             map.get(prerequisites[i][1]).add(prerequisites[i][0]);
-        }
-    }
-    
-    public void getInDegree(int[][] prerequisites, int numCourses) {
-        for (int i = 0; i < numCourses; i++) {
-            inorder.put(i, 0);
-        }
-        for (int i = 0; i < prerequisites.length; i++) {
-            inorder.put(prerequisites[i][0], inorder.get(prerequisites[i][0]) + 1);
         }
     }
 }
