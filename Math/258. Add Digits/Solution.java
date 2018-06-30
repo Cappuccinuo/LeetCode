8696f226eb975b881bd0971a11c229d8395c3405
Yuan Gao <gaoyuanbj@yahoo.com>
2018-06-30 03:21:28 -0400

    [REVIEW][Math] #258 Add Digits

--- Math/258. Add Digits/Solution.java
+++ Math/258. Add Digits/Solution.java
@@ -3,3 +3,31 @@ class Solution {
         return 1 + (num - 1) % 9;
     }
 }
+
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+}
+
+class Solution {
+    public int addDigits(int num) {
+        String numString = String.valueOf(num);
+        if (numString.length() == 1) {
+            return num;
+        }
+        while (numString.length() != 1) {
+            int len = numString.length();
+            int result = 0;
+            for (int i = len - 1; i >= 0; i--) {
+                result += numString.charAt(i) - '0';
+            }
+            numString = String.valueOf(result);
+        }
+        
+        return Integer.parseInt(numString);
+    }
+}
