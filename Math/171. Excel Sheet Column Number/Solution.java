1e458879a42e85022188f8cb810578426bc1d370
Yuan Gao <gaoyuanbj@yahoo.com>
2018-06-30 04:13:00 -0400

    [REVIEW][Math] 13 + 171

--- Math/171. Excel Sheet Column Number/Solution.java
+++ Math/171. Excel Sheet Column Number/Solution.java
@@ -1,3 +1,15 @@
+// 2018.6.30
+class Solution {
+    public int titleToNumber(String s) {
+        int len = s.length();
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            result += Math.pow(26, i) * (s.charAt(len - i - 1) - 'A' + 1);
+        }
+        return result;
+    }
+}
+
 class Solution {
     public int titleToNumber(String s) {
         int result = 0;
