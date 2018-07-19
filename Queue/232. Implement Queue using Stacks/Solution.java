a2e6e0b3cb5e5b7158b9e043e5e98993f21a693c
Yuan Gao <gaoyuanbj@yahoo.com>
2018-07-19 03:26:13 -0400

    README

--- Queue/232. Implement Queue using Stacks/Solution.java
+++ Queue/232. Implement Queue using Stacks/Solution.java
@@ -1,3 +1,60 @@
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    int front;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if (stack1.isEmpty()) {
+            front = x;
+        }
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }    
+        }
+        int top = stack2.pop();
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (!empty()) {
+            front = stack2.peek();
+        }
+        return top;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return front;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
 // Using two stacks, push O(1), pop O(1)
 class MyQueue {
     Stack<Integer> s1;
