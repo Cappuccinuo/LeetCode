cc336f80932daa6e6d1fceff8651b0d488be65a4
Yuan Gao <gaoyuanbj@yahoo.com>
2018-07-25 04:26:54 -0400

    Code

--- Array/540. Single Element in a Sorted Array/Solution.java
+++ Array/540. Single Element in a Sorted Array/Solution.java
@@ -7,3 +7,27 @@ class Solution {
         return mask;
     }
 }
+
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid % 2 != 0) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                end = mid;
+            }
+            else {
+                start = mid + 2;
+            }
+        }
+        
+        return nums[start];
+    }
+}
