d618ac832dacb256d50020eee292a9bbd833d4ee
Yuan Gao <gaoyuanbj@yahoo.com>
2018-09-24 01:00:47 -0400

    Review of 5,6,11,12,15,16

--- String/6. ZigZag Conversion/Solution.java
+++ String/6. ZigZag Conversion/Solution.java
@@ -28,3 +28,33 @@ class Solution {
         return(sb.toString());
     }
 }
+
+//2018.9.23
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        int len = s.length();
+        int i = 0;
+        
+        for (int j = 0; j < numRows; j++) {
+            sbs[j] = new StringBuilder();
+        }
+        
+        while (i < len) {
+            for (int index = 0; index < numRows && i < len; index++) {
+                sbs[index].append(s.charAt(i));
+                i++;
+            }
+            for (int index = numRows - 2; index >= 1 && i < len; index--) {
+                sbs[index].append(s.charAt(i));
+                i++;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < numRows; j++) {
+            sb.append(sbs[j]);
+        }
+        return sb.toString();
+    }
+}
