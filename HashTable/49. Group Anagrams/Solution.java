e74f20ade4fe40c64e8896e53f1923ee1f1b7ecf
Yuan Gao <gaoyuanbj@yahoo.com>
2018-10-02 00:16:21 -0400

    README

--- HashTable/49. Group Anagrams/Solution.java
+++ HashTable/49. Group Anagrams/Solution.java
@@ -26,3 +26,42 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new LinkedList<>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+        
+        Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+        
+        for (String str : strs) {
+            Arrays.fill(count, 0);
+            
+            char[] letters = str.toCharArray();
+            for (char letter : letters) {
+                count[letter - 'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append(count[i]);
+                sb.append("#");
+            }
+            
+            String key = sb.toString();
+            if (!map.containsKey(key)) {
+                map.put(key, new LinkedList<>());
+            }
+            map.get(key).add(str);
+        }
+        
+        for (String k : map.keySet()) {
+            result.add(map.get(k));
+        }
+        
+        return result;
+    }
+}
