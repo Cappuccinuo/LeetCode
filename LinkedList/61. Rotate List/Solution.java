3f8bd0c8f657b079fe91ffac0bb4dcd1b4030def
Yuan Gao <gaoyuanbj@yahoo.com>
2018-10-02 18:49:28 -0400

    README

--- LinkedList/61. Rotate List/Solution.java
+++ LinkedList/61. Rotate List/Solution.java
@@ -8,6 +8,45 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        
+        int len = 0;
+        while (fast.next != null) {
+            fast = fast.next;
+            len++;
+        }
+        
+        int step = len - k % len;
+        while (step > 0) {
+            slow = slow.next;
+            step--;
+        }
+        
+        fast.next = dummy.next;
+        dummy.next = slow.next;
+        slow.next = null;
+        
+        return dummy.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null) {
             return head;
         }
