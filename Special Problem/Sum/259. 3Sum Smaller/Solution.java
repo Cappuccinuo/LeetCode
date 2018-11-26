df8e682f8a8ec16b927e1eb33605b7b385a9e2cb
Yuan Gao <gaoyuanbj@yahoo.com>
2018-11-26 17:00:08 -0500

    3Sum Smaller

--- Special Problem/Sum/259. 3Sum Smaller/Solution.java
+++ Special Problem/Sum/259. 3Sum Smaller/Solution.java
@@ -1,6 +1,36 @@
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
+        int count = 0;
+        int len = nums.length;
+        for (int i = 0; i < len - 2; i++) {
+            count += twoSumHelper(nums, i + 1, target - nums[i]);
+        }
+        return count;
+    }
+    
+    private int twoSumHelper(int[] nums, int index, int limit) {
+        int start = index;
+        int end = nums.length - 1;
+        int count = 0;
+        
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum < limit) {
+                count += end - start;
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        return count;
+    }
+}
+
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
         int numsLen = nums.length;
         int count = 0;
         for (int i = 0; i < numsLen - 2; i++) {
