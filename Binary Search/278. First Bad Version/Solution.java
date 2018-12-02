b2a283d665b2a11930c918ae233e660bca16cdb6
Yuan Gao <gaoyuanbj@yahoo.com>
2018-12-01 20:15:18 -0500

    First bad version

--- Binary Search/278. First Bad Version/Solution.java
+++ Binary Search/278. First Bad Version/Solution.java
@@ -19,3 +19,30 @@ public class Solution extends VersionControl {
         return lo;
     }
 }
+// 2018.11.7
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            }
+            else {
+                start = mid;   
+            }
+        }
+        
+        if (isBadVersion(start)) {
+            return start;
+        }
+        else if (isBadVersion(end)) {
+            return end;
+        }
+        throw new java.util.NoSuchElementException("All versions are good.");
+    }
+}
