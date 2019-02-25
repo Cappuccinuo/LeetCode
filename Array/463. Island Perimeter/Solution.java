48513239618209bca00537b328aac481bc65beff
Yuan Gao <gaoyuanbj@yahoo.com>
2019-02-24 23:16:12 -0500

    463. Island Perimeter

--- Array/463. Island Perimeter/Solution.java
+++ Array/463. Island Perimeter/Solution.java
@@ -17,3 +17,51 @@ class Solution {
         return perimeter;
     }
 }
+
+class Solution {
+    int row, col;
+    
+    public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        row = grid.length;
+        col = grid[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    return DFS(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+    
+    private int DFS(int[][] grid, int i, int j) {
+        if (!inBound(i, j)) {
+            return 1;
+        }
+        
+        if (grid[i][j] == 0) {
+            return 1;
+        }
+        
+        if (grid[i][j] == -1) {
+            return 0;
+        }
+        int count = 0;
+        grid[i][j] = -1;
+        
+        count += DFS(grid, i + 1, j);
+        count += DFS(grid, i - 1, j);
+        count += DFS(grid, i, j + 1);
+        count += DFS(grid, i, j - 1);
+        
+        return count;
+    }
+    
+    private boolean inBound(int i, int j) {
+        return i >= 0 && i < row && j >= 0 && j < col;
+    }
+}
