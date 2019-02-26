ac30b0d8d531db16ea590ca2fde6ae1004205d97
Yuan Gao <gaoyuanbj@yahoo.com>
2019-02-25 21:46:43 -0500

    375

--- Dynamic Programming/375. Guess Number Higher or Lower II/Solution.java
+++ Dynamic Programming/375. Guess Number Higher or Lower II/Solution.java
@@ -2,16 +2,22 @@ class Solution {
     public int getMoneyAmount(int n) {
         int[][] dp = new int[n + 1][n + 1];
         int l, i, j, k;
+        for (i = 1; i <= n; i++) {
+            dp[i][i] = 0;
+        }
         for (l = 2; l <= n; l++) {
             for (i = 1; i <= n - l + 1; i++) {
                 j = i + l - 1;
                 dp[i][j] = Integer.MAX_VALUE;
-                for (k = i; k <= j; k++) {
-                    dp[i][j] = Math.min(Math.max(k > 1 ? dp[i][k - 1] : 0, 
-                                                 k < n ? dp[k + 1][j] : 0) + k, dp[i][j]);
+                // We can just start at i + (l - 1) / 2, as left is always smaller than right
+                for (k = i + (l - 1) / 2; k <= j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], 
+                                        Math.max(k > 1 ? dp[i][k - 1] : 0, 
+                                                 k < n ? dp[k + 1][j] : 0) + k);
                 }
             }
         }
+        
         return dp[1][n];
     }
 }
