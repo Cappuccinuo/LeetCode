0371e496a4dc9ac27e883eb460a32063d730153f
Yuan Gao <gaoyuanbj@yahoo.com>
2019-03-04 16:22:30 -0500

    909. Snakes and Ladders

--- BFS/909. Snakes and Ladders/Solution.java
+++ BFS/909. Snakes and Ladders/Solution.java
@@ -1,3 +1,83 @@
+// 2019.3.4
+class Solution {
+    int N;
+    
+    public int snakesAndLadders(int[][] board) {
+        N = board.length;
+        int i, j;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N * N + 1];
+        
+        queue.offer(1);
+        visited[1] = true;
+        int step = 0;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for (i = 0; i < size; i++) {
+                int front = queue.poll();
+                if (front == N * N) {
+                    return step;
+                }
+                
+                for (j = 1; j <= 6; j++) {
+                    int new_index = front + j;
+                    int[] ind = getIndex(new_index);
+                    if (!inBound(ind[0], ind[1])) {
+                        continue;
+                    }
+                    int val = board[ind[0]][ind[1]];
+                    if (!visited[new_index]) {
+                        if (val != -1) {
+                            queue.offer(val);
+                        }
+                        else {
+                            queue.offer(new_index);
+                        }
+                        
+                        visited[new_index] = true;
+                    }
+                }
+            }
+            
+            step++;
+        }
+        
+        return -1;
+    }
+    
+    private boolean inBound(int i, int j) {
+        return i >= 0 && i < N && j >= 0 && j < N;
+    }
+    
+    private int[] getIndex(int number) {
+        if (number < 1 || number > N * N) {
+            return new int[]{-1, -1};
+        }
+        int row = N - 1 - (number - 1) / N;
+        int col = 0;
+        if (N % 2 == 0) {
+            if (row % 2 == 1) {
+                col = (number - 1) % N;
+            }
+            else {
+                col = N - 1 - (number - 1) % N;
+            }
+        }
+        else {
+            if (row % 2 == 0) {
+                col = (number - 1) % N;
+            }
+            else {
+                col = N - 1 - (number - 1) % N;
+            }
+        }
+        return new int[]{row, col};
+    }
+}
+
+
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
