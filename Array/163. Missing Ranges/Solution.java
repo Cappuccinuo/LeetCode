e72992ae17a06ed1223e6b373d3928f0c65f8d78
Yuan Gao <gaoyuanbj@yahoo.com>
2019-03-27 09:28:54 -0400

    Update Solution.java

--- Array/163. Missing Ranges/Solution.java
+++ Array/163. Missing Ranges/Solution.java
@@ -6,24 +6,17 @@ class Solution {
         int len = nums.length;
         long start = (long)lower - 1;
         
-        for (i = 0; i < len; i++) {
+        for (i = 0; i <= len; i++) {
             long left = start + 1;
-            long right = (long)nums[i] - 1;
+            long right = i == len ? upper : (long)nums[i] - 1;
             if (left == right) {
                 result.add(left + "");
             }
             else if (left < right) {
                 result.add(left + "->" + right);
             }
-            start = nums[i];
-        }
-        
-        if (start < upper) {
-            if (start + 1 == upper) {
-                result.add(upper + "");
-            }
-            else {
-                result.add((start + 1) + "->" + upper);
+            if (i < len) {
+                start = nums[i];
             }
         }
         
