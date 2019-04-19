0f7aaf5f45473d3b4247d655d18b8857e1a2ef32
Yuan Gao <gaoyuanbj@yahoo.com>
2019-04-19 10:42:53 -0400

    387. First Unique Character in a String

--- String/387. First Unique Character in a String/README.md
+++ String/387. First Unique Character in a String/README.md
@@ -29,3 +29,7 @@ DESIGN STRATEGY: Use HashMap to store the times of each character, then traverse
 
 
 2018.4.13 Faster: We can traverse the 26 characters, for every character, we use indexOf to get its current index in the string, if it exist, we use lastIndexOf to get its last index in the string, compare the two, update the firstIndex variable.
+
+
+
+2019.4.19 Data Stream: Use the idea of LRU. We store the char and index in a ListCharNode, maintain a list of frequency 1 character. When the character is visited twice, we remove it from the list.
--- String/387. First Unique Character in a String/Solution.java
+++ String/387. First Unique Character in a String/Solution.java
@@ -1,4 +1,4 @@
-// 2018.4.13
+// 2019.4.19
 class Solution {
     public int firstUniqChar(String s) {
         int firstIndex = s.length();
@@ -28,5 +28,71 @@ class Solution {
             }
         }
         return index;
+    }
+}
+
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] array = s.toCharArray();
+        DataStream ds = new DataStream();
+        int i;
+        int len = array.length;
+        for (i = 0; i < len; i++) {
+            ds.add(array[i], i);
+        }
+        return ds.getFirstUniqueCharIndex();
+    }
+}
+
+class DataStream {
+    ListCharNode dummy;
+    ListCharNode tail;
+    Map<Character, ListCharNode> mapToPrev;
+    boolean[] visited;
+    
+    DataStream() {
+        dummy = new ListCharNode('#', -1);
+        tail = dummy;
+        mapToPrev = new HashMap<>();
+        visited = new boolean[26];
+    }
+    
+    void add(char c, int i) {
+        if (!mapToPrev.containsKey(c)) {
+            ListCharNode newNode = new ListCharNode(c, i);
+            mapToPrev.put(c, tail);
+            tail.next = newNode;
+            tail = tail.next;
+            return;
+        }
+
+        if (!visited[c - 'a']) {
+            visited[c - 'a'] = true;
+            ListCharNode prev = mapToPrev.get(c);
+            prev.next = prev.next.next;
+            if (prev.next == null) {
+                tail = prev;
+            }
+            else {
+                mapToPrev.put(prev.next.c, prev);
+            }
+        }
+    }
+    
+    int getFirstUniqueCharIndex() {
+        if (dummy.next == null) {
+            return -1;
+        }
+        return dummy.next.index;
+    }
+}
+
+class ListCharNode {
+    char c;
+    int index;
+    ListCharNode next;
+    ListCharNode(char c, int i) {
+        this.c = c;
+        this.index = i;
     }
 }
