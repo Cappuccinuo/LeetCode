4590d71df8e0cabe47145cecfac55719b07447c7
Yuan Gao <gaoyuanbj@yahoo.com>
2019-04-19 15:59:55 -0400

    91. Decode Ways

--- Dynamic Programming/91. Decode Ways/Solution.java
+++ Dynamic Programming/91. Decode Ways/Solution.java
@@ -25,3 +25,26 @@ class Solution {
         return dp[0];
     }
 }
+
+class Solution {
+    public int numDecodings(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1; 
+        
+        int i;
+        for (i = 2; i <= len; i++) {
+            String first = s.substring(i - 1, i);
+            String second = s.substring(i - 2, i);
+            if (Integer.parseInt(first) >= 1 && Integer.parseInt(first) <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (Integer.parseInt(second) >= 10 && Integer.parseInt(second) <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[len];
+    }
+}
