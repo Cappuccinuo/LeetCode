8c41d909b733bdc05d6dd61d378ceab7f71e59fc
Yuan Gao <gaoyuanbj@yahoo.com>
2019-04-21 11:39:45 -0400

    695. Max Area Of Island

--- DFS/695. Max Area of Island/Solution.java
+++ DFS/695. Max Area of Island/Solution.java
@@ -1,23 +1,35 @@
 class Solution {
+    private static final int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
     public int maxAreaOfIsland(int[][] grid) {
-        int maxarea = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int i, j;
+        int maxArea = 0;
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    maxarea = Math.max(maxarea, areaOfIsland(grid, i, j));
+                    maxArea = Math.max(DFS(grid, i, j, m, n), maxArea);
                 }
             }
         }
-        return maxarea;
+        return maxArea;
     }
     
-    public int areaOfIsland(int[][] grid, int i, int j) {
-        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
-            grid[i][j] = 0;
-            int area = areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i - 1, j) 
-                + areaOfIsland(grid, i, j - 1) + areaOfIsland(grid, i, j + 1) + 1;
-            return area;
+    private int DFS(int[][] grid, int i, int j, int m, int n) {
+        if (!inBound(i, j, m, n) || grid[i][j] == 0) {
+            return 0;
         }
-        return 0;
+        
+        grid[i][j] = 0;
+        int area = 1;
+        for (int[] direction : DIRECTIONS) {
+            area += DFS(grid, i + direction[0], j + direction[1], m, n);
+        }
+        return area;
+    }
+    
+    private boolean inBound(int i, int j, int m, int n) {
+        return i >= 0 && i < m && j >= 0 && j < n;
     }
 }
--- Special Problem/Sliding Window/438. Find All Anagram/Solution.java
+++ Special Problem/Sliding Window/438. Find All Anagram/Solution.java
@@ -26,3 +26,39 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // Final Result
+        List<Integer> result = new LinkedList<>();
+        // Two Pointers, move end
+        int start = 0;
+        int end = 0;
+        // character frequency of s
+        int[] sMap = new int[26];
+        // character frequency of p
+        int[] pMap = new int[26];
+        for (char pLetter : p.toCharArray()) {
+            pMap[pLetter - 'a']++;
+        }
+        // Len of s
+        int len = s.length();
+        
+        while (end < len) {
+            char sLetter = s.charAt(end);
+            end++;
+            sMap[sLetter - 'a']++;
+            
+            while (pMap[sLetter - 'a'] < sMap[sLetter - 'a']) {
+                char sLetterStart = s.charAt(start);
+                start++;
+                sMap[sLetterStart - 'a']--;
+            }
+            
+            if (end - start == p.length()) {
+                result.add(start);
+            }
+        }
+        return result;
+    }
+}
