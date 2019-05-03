aab92cc12fcd93eabb183843c58b344300aa0097
Yuan Gao <gaoyuanbj@yahoo.com>
2019-05-03 17:30:20 -0400

    674.java update

--- Array/674. Longest Continuous Increasing Subsequence/Solution.java
+++ Array/674. Longest Continuous Increasing Subsequence/Solution.java
@@ -18,3 +18,42 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = 1;
+        int prev = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                max = Math.max(i - prev + 1, max);
+            }
+            else {
+                prev = i;
+            }
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int start = 0;
+        int end = 0;
+        int res = 0;
+        while (end < len) {
+            if (end > 0 && nums[end] <= nums[end - 1]) {
+                start = end;
+            }
+            end++;
+            res = Math.max(res, end - start);
+        }
+        return res;
+    }
+}
