a7d027280b1bc7cf30e69147cf93706a4243605d
Yuan Gao <gaoyuanbj@yahoo.com>
2020-03-15 20:09:17 -0400

    1381   Lazy increment

--- Stack/1381. Design a Stack With Increment Operation/Solution.java
+++ Stack/1381. Design a Stack With Increment Operation/Solution.java
@@ -1,4 +1,53 @@
 class CustomStack {
+    Stack<Integer> stack;
+    int maxSize;
+    int[] inc;
+    
+    public CustomStack(int maxSize) {
+        this.stack = new Stack<>();
+        this.maxSize = maxSize;
+        this.inc = new int[maxSize];
+    }
+    
+    public void push(int x) {
+        if (stack.size() == maxSize) {
+            return;
+        }
+        stack.push(x);
+    }
+    
+    public int pop() {
+        if (stack.size() == 0) {
+            return -1;
+        }
+        int currentIndex = stack.size() - 1;
+        int top = stack.pop() + inc[currentIndex];
+        if (currentIndex - 1 >= 0) {
+            inc[currentIndex - 1] += inc[currentIndex];
+        }
+        inc[currentIndex] = 0;
+        return top;
+    }
+    
+    public void increment(int k, int val) {
+        int i = Math.min(k, stack.size()) - 1;
+        if (i >= 0) {
+            inc[i] += val;
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
+
+
+
+class CustomStack {
     List<Integer> stack;
     int maxSize;
     
