dd021052ee7830e45b48a806399fcc59b182d533
Yuan Gao <gaoyuanbj@yahoo.com>
2020-09-14 18:15:01 +0800

    Update Solution.java

--- Array/57. Insert Interval/Solution.java
+++ Array/57. Insert Interval/Solution.java
@@ -28,3 +28,25 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        
+        for (int[] interval : intervals) {
+            if (interval[1] < newInterval[0]) {
+                result.add(interval);
+            } else if (newInterval[1] < interval[0]) {
+                result.add(newInterval);
+                newInterval = interval;
+            } else {
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+        }
+        
+        result.add(newInterval);
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
