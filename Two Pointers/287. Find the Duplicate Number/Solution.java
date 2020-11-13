f7bb8fca12c1a6026d36b6f835af74dda42b11b2
Yuan Gao <gaoyuanbj@yahoo.com>
2020-11-13 13:41:55 +0800

    Update a method with binary search

--- Two Pointers/287. Find the Duplicate Number/Solution.java
+++ Two Pointers/287. Find the Duplicate Number/Solution.java
@@ -1,6 +1,10 @@
 // Two Pointers slow and fast to find cycle
 class Solution {
     public int findDuplicate(int[] nums) {
+        // 1 -> 2 -> 3 -> 4 -> 1
+        //      ^              |
+        //      |              |
+        //      ----------------
         // slow and fast pointer to prove that the nums array has cycle
         int slow = nums[0];
         int fast = nums[0];
@@ -15,6 +19,36 @@ class Solution {
             fast = nums[fast];
         }
         return slow;
+    }
+}
+
+// Two pointers binary search
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int start = 1;
+        int end = nums.length;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (countNumberEqualOrSmallerThan(nums, mid) > mid) {
+                end = mid;;
+            } else {
+                start = mid;
+            }
+        }
+        if (countNumberEqualOrSmallerThan(nums, start) > start) {
+            return start;
+        }
+        return end;
+    }
+    
+    private int countNumberEqualOrSmallerThan(int[] nums, int target) {
+        int count = 0;
+        for (int n : nums) {
+            if (n <= target) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
